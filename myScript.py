5e05884b931c54e277591f04858748320c425064
Peter <sqiu@ualberta.ca>
2023-01-09 18:16:11 -0700

    Update myScript.py

--- myScript.py
+++ myScript.py
@@ -1,3 +1,14 @@
 import requests
-r = requests.get("https://google.com")
-print(r)
+from os import getcwd
+
+url = "https://raw.githubusercontent.com/riceboypeter/CMPUT-404-lab-1/main/myScript.py"
+d = getcwd()
+filename = d + "myScript.py"
+r = requests.get(url)
+
+f = open(filename,'w')
+f.write(r.text)
+f.close()
+
+f = open(filename, 'r')
+print(f.read())
